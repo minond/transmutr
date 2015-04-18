90c0623bfdab10b3c6b68c09767cab97ae17ba36
Sean Brown <inphomercial@gmail.com>
2015-04-17 22:39:47 -0600

    added document event listener

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 // https://developer.chrome.com/extensions/examples/tutorials/getstarted/popup.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Starting Extension");
+});
