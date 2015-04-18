1ed3512b4008e049be4f32b6b105a1a8a496e5b4
Marcos Minond <minond.marcos@gmail.com>
2015-04-18 00:32:07 -0600

    parsing spotify and google play urls

--- background.js
+++ background.js
@@ -1,9 +1,70 @@
-// https://developer.chrome.com/extensions/background_pages
-chrome.webRequest.onBeforeRequest.addListener(function (req) {
+var CHECK_GOOGLE = /play.google/,
+    CHECK_SPOTIFY = /play.spotify/;
+
+var ID_GOOGLE = /preview\/([\w\d]+)/,
+    ID_SPOTIFY = /track\/([\w\d]+)/;
+
+var SERVICE_GOOGLE = 'google',
+    SERVICE_SPOTIFY = 'spotify';
+
+/**
+ * takes a url and returns the service it's from and the id of the track
+ *
+ * example input:
+ * https://play.spotify.com/track/1m85PpnCEa9EaU2z6lGwmO
+ * https://play.google.com/music/preview/Th73lqpq5emphzzyg4mqgt4fwnu
+ *
+ * example output:
+ * { service: 'spotify', id: '1m85PpnCEa9EaU2z6lGwmO' }
+ * { service: 'google', id: 'Th73lqpq5emphzzyg4mqgt4fwnu' }
+ *
+ * @param {String} url
+ * @return {Object}
+ */
+function parse(url) {
+    function id(parser) {
+        var parts = url.match(parser);
+        return parts && parts.pop();
+    }
+
+    switch (true) {
+        case CHECK_SPOTIFY.test(url):
+            return {
+                service: SERVICE_SPOTIFY,
+                id: id(ID_SPOTIFY)
+            };
+            break;
+
+        case CHECK_GOOGLE.test(url):
+            return {
+                service: SERVICE_GOOGLE,
+                id: id(ID_GOOGLE)
+            };
+            break;
+    }
+}
+
+function incoming_request(req) {
     console.log('%s => %s', req.method, req.url);
-}, {
+    console.log(parse(req.url));
+
+    // var info = parse(req.url);
+    // var user = get_user_info();
+    //
+    // // info.service will be spotify, google, etc.
+    // var incoming_service = get_integration_for(info.service);
+    // var outgoing_service = get_integration_for(user.service);
+    //
+    // // info.id will be the track id we got from the url
+    // var track = incoming_service.get_track_info(info.id)
+    //
+    // console.log(outgoing_service.find_track_url(track));
+}
+
+// https://developer.chrome.com/extensions/background_pages
+chrome.webRequest.onBeforeRequest.addListener(incoming_request, {
     urls: [
-        "*://*.spotify.com/track/*",
-        "*://play.google.com/music/*"
+        '*://*.spotify.com/track/*',
+        '*://play.google.com/music/*'
     ]
 }, []);
