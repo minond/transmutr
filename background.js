a4e466888948553ee54b8ef6b7d47c7efb2c8372
Marcos Minond <minond.marcos@gmail.com>
2015-04-17 23:33:10 -0600

    logging requests to google

---
+++ background.js
@@ -0,0 +1,7 @@
+// https://developer.chrome.com/extensions/background_pages
+chrome.webRequest.onBeforeRequest.addListener(function (req) {
+    // console.log(arguments);
+    console.log('%s => %s', req.method, req.url);
+}, {
+    urls: ["*://*.google.com/*"]
+}, []);
