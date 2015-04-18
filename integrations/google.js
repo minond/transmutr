2bbe1c52fbb9163d4bfbb21c8dde3ba96b57e86e
Sam Beveridge <sam.bev87@gmail.com>
2015-04-18 14:55:12 -0600

    Added the spotify -> google transmutr.

--- integrations/google.js
+++ integrations/google.js
@@ -6,7 +6,7 @@ integration('google', {
     id: /preview\/([\w\d]+)/,
 
     get_track_info: function (url, callback) {
-        var search_url = 'https://play.google.com/music/m/',
+        var track_url = 'https://play.google.com/music/m/',
             parts = url.match(this.id);
 
         if (!parts) {
@@ -14,7 +14,7 @@ integration('google', {
             return;
         }
 
-        http_get(search_url + parts[1], function (res) {
+        http_get(track_url + parts[1], function (res) {
             console.log('response from google:', res.responseText);
             var wrapper = document.createElement('div');
             wrapper.innerHTML = res.responseText;
@@ -26,6 +26,24 @@ integration('google', {
                 title: title_info[0],
                 artist: title_info[1]
             });
+        });
+    },
+
+    get_track_url: function (track, callback) {
+        var search_url = 'https://play.google.com/store/search?q=',
+            search_end = '&c=music',
+            track_url = 'https://play.google.com/music/m/';
+
+        search_url += encodeURIComponent(track.artist + ' ');
+        search_url += encodeURIComponent(track.title);
+
+        http_get(search_url + search_end, function (res) {
+            var wrapper = document.createElement('div');
+            wrapper.innerHTML = res.responseText;
+            var song_info = wrapper
+                .querySelector('div.card.no-rationale.square-cover.music.small')
+
+            callback(track_url + song_info.getAttribute('data-docid').slice(5))
         });
     }
 });
