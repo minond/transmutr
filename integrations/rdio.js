9657f1d0371e63139001f11af7b95825c12f1611
DeanC <deanc@pa-14.home>
2015-04-18 23:01:58 -0600

    Doing the work Marcos was too lazy to do

--- integrations/rdio.js
+++ integrations/rdio.js
@@ -20,5 +20,14 @@ integration('rdio', {
     },
 
     get_track_url: function (track, callback) {
+        // http://www.rdio.com/artist/Lord_Huron/album/Strange_Trails/track/Until_the_Night_Turns/
+        var url = 'http://www.rdio.com/artist/' +
+            encodeURIComponent(track.artist) +
+            /album/ +
+            encodeURIComponent(track.album) +
+            /track/ +
+            encodeURIComponent(track.title);
+
+        callback(url);
     }
 });
