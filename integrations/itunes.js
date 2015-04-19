063f58e0c709dc155bd8b73bea291a27190b6629
DeanC <deanc@pa-14.home>
2015-04-18 18:51:15 -0600

    Adding itunes integration

---
+++ integrations/itunes.js
@@ -0,0 +1,38 @@
+'use strict';
+
+integration('itunes', {
+
+    urls: ['*://itunes.apple.com/us/album*'],
+    checker: /itunes.apple/,
+    id: /album\/.+\/([\w\d]+)(.{0,}[&|\?]i=([\w\d]+))?/,
+
+    get_track_info: function (url, callback) {
+        var id = url.match(this.id)[3];
+        var req = new XMLHttpRequest();
+
+        url = 'https://itunes.apple.com/lookup?id=' + id;
+
+        req.onload = function (res) {
+            var result = JSON.parse(req.responseText).results[0];
+            callback({
+                title: result.trackName,
+                artist: result.artistName,
+                album: result.collectionName
+            })
+        }
+
+        req.open('GET', url, true);
+        req.send();
+    },
+
+    get_track_url: function (track, callback) {
+        var search_url = 'https://itunes.apple.com/search?entity=song&limit=1&term=' +
+            encodeURIComponent(track.artist) + '+' +
+            encodeURIComponent(track.title);
+
+        http_get(search_url, function (res) {
+            var result = JSON.parse(res.responseText);
+            callback(result.results[0].trackViewUrl);
+        })
+    }
+});
