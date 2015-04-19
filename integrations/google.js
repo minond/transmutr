d74e6b64ad28c5555d118b68a5dcf1f77baf0ff5
DeanC <deanc@pa-14.home>
2015-04-19 17:49:14 -0600

    added google user url regex. Better error handling

--- integrations/google.js
+++ integrations/google.js
@@ -3,7 +3,7 @@
 integration('google', {
     urls: ['*://play.google.com/music/*'],
     checker: /play.google/,
-    id: /preview\/([\w\d]+)/,
+    id: /music\/[m|preview]+\/([\w\d]+)/,
 
     get_track_info: function (url, callback) {
         var track_url = 'https://play.google.com/music/m/',
@@ -14,13 +14,19 @@ integration('google', {
             return;
         }
 
+        console.log(url);
         http_get(track_url + parts[1], function (res) {
             console.log('response from google:', res.responseText);
             var wrapper = document.createElement('div');
             wrapper.innerHTML = res.responseText;
 
-            var title_info = wrapper.querySelector('meta[property="og:title"]')
-                .getAttribute('content').split(' - ');
+            var title_el = wrapper.querySelector('meta[property="og:title"]')
+            if(!title_el) {
+                callback(null);
+                return;
+            }
+
+            var title_info = title_el.getAttribute('content').split(' - ');
 
             callback({
                 title: title_info[0],
@@ -42,6 +48,11 @@ integration('google', {
             wrapper.innerHTML = res.responseText;
             var song_info = wrapper
                 .querySelector('div.card.no-rationale.square-cover.music.small')
+
+            if(!song_info){
+                callback(null);
+                return;
+            }
 
             callback(track_url + song_info.getAttribute('data-docid').slice(5))
         });
