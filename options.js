fd9b2b58e6687dff10d8758e97b03b379af7d1b9
Sam Beveridge <sam.bev87@gmail.com>
2015-04-18 23:56:24 -0600

    Fixed save button css and js regressions from change.

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    var form = document.getElementById('saveform'),
+    var save_btn = document.getElementById('savebtn'),
         primaryselect = document.getElementById('primaryStream'),
         secondaryselect = document.getElementById('secondaryStream'),
         open_in = document.getElementById('newtab'),
@@ -10,12 +10,12 @@ window.onload = function () {
     secondaryselect.value = user_info.secondary_service || 'spotify';
     open_in.checked = user_info.open_in == 'new' ? true : false;
 
-    form.onsubmit = function (ev) {
+    save_btn.addEventListener('click', function (ev) {
         // update user info and save it to localstorage
         user_info.service = primaryselect.value;
         user_info.secondary_service = secondaryselect.value;
         user_info.open_in = open_in.checked ? 'new' : 'current';
         set_user_info(user_info)
         return false;
-    }
+    });
 }
