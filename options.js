497b03f68b5c1bc54edc4f7006a80a36c0710b86
Sean Brown <inphomercial@gmail.com>
2015-04-26 14:26:16 -0600

    Added a close button when saving your options. faster and easier to find than having to go click the window X to close.

--- options.js
+++ options.js
@@ -1,5 +1,6 @@
 window.onload = function () {
     var save_btn = document.getElementById('savebtn'),
+        close_btn = document.getElementById('closebtn'),
         primaryselect = document.getElementById('primaryStream'),
         open_in = document.getElementById('newtab'),
         user_info = get_user_info() || {};
@@ -8,12 +9,20 @@ window.onload = function () {
     primaryselect.value = user_info.service;
     open_in.checked = user_info.open_in == 'new' ? true : false;
 
+    close_btn.addEventListener('click', function (ev) {
+        window.close();
+    });
+
     save_btn.addEventListener('click', function (ev) {
         // update user info and save it to localstorage
         user_info.service = primaryselect.value;
         user_info.open_in = open_in.checked ? 'new' : 'current';
         set_user_info(user_info)
         save_btn.innerHTML = 'Saved!';
+
+        // Doesnt work with IE9<
+        close_btn.classList.remove('invisible');
+       
         return false;
     });
 }
